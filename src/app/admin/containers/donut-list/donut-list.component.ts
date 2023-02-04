59a31dc954f15a6a6551d4d7ed3b3af33b601267
otoniricardo <otoniricardo@gmail.com>
2023-02-04 11:04:00 -0300

    [Component Architecture]: Type definitions with interfaces

--- src/app/admin/containers/donut-list/donut-list.component.ts
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Donut } from '../../models/donut.model';
 
 @Component({
   selector: 'app-donut-list',
@@ -13,8 +14,8 @@ import { Component, OnInit } from '@angular/core';
   styles: [],
 })
 export class DonutListComponent implements OnInit {
-  donuts!: any[];
-  donut!: any;
+  donuts!: Donut[];
+  donut!: Donut;
 
   constructor() {}
 
