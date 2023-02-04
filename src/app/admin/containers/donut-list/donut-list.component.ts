8e89fed0dd68db407c48ada4e0bf99c6fb6bf67f
otoniricardo <otoniricardo@gmail.com>
2023-02-04 10:53:58 -0300

    [Component Architecture]: Use the NgOnInit lifecycle Hook

--- src/app/admin/containers/donut-list/donut-list.component.ts
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -1,8 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-donut-list',
-  template: ` <p>donut-list works!</p> `,
+  template: `
+    <div>
+      <div>
+        {{ donut.name }}
+        {{ donut.price }}
+      </div>
+    </div>
+  `,
   styles: [],
 })
-export class DonutListComponent {}
+export class DonutListComponent implements OnInit {
+  donuts!: any[];
+  donut!: any;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.donuts = [
+      {
+        id: 1,
+        name: 'Just chocolate',
+        icon: 'just-chocolate',
+        price: 119,
+        description: 'For the pure chocolate',
+      },
+      {
+        id: 2,
+        name: 'Glazed fudge',
+        icon: 'glazed-fusge',
+        price: 129,
+        description: 'Sticky perfection',
+      },
+      {
+        id: 3,
+        name: 'Caramel swirl',
+        icon: 'caramel-swirl',
+        price: 129,
+        description: 'Chocolate drizzled with caramel',
+      },
+    ];
+    this.donut = this.donuts[0];
+  }
+}
