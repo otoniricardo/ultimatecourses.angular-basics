e288d44d275fce3560e02b5739268723805fd991
otoniricardo <otoniricardo@gmail.com>
2023-02-04 23:58:00 -0300

    [Template Forms and validation]: <input> binding with NgModel

--- src/app/admin/components/donut-form/donut-form.component.ts
+++ src/app/admin/components/donut-form/donut-form.component.ts
@@ -4,6 +4,14 @@ import { Component } from '@angular/core';
   selector: 'app-donut-form',
   template: `
     <form class="donut-form" #form="ngForm">
+      <label>
+        <span>Name</span>
+        <input type="text" name="name" class="input" ngModel />
+      </label>
+      <label>
+        <span>Price</span>
+        <input type="number" name="price" class="input" ngModel />
+      </label>
       <pre> {{ form.value | json }} </pre>
     </form>
   `,
