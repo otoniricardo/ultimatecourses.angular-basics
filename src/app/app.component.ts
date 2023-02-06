0edc05d7138d095591dd8f4bbc3638f84b91acb7
otoniricardo <otoniricardo@gmail.com>
2023-02-06 19:59:02 -0300

    [Routing and Navigation]: Root Module Routes with forRoot()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
       <header class="header">
         <img src="/assets/img/logo.svg" alt="Ultimate Donuts" class="logo" />
       </header>
-      <app-donut-single></app-donut-single>
+      <router-outlet></router-outlet>
     </div>
   `,
   styles: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AdminModule } from './admin/admin.module';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DonutListComponent } from './admin/containers/donut-list/donut-list.component';
+import { DonutSingleComponent } from './admin/containers/donut-single/donut-single.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: DonutListComponent,
+  },
+  {
+    path: 'donut',
+    component: DonutSingleComponent,
+  },
+];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AdminModule],
+  imports: [BrowserModule, RouterModule.forRoot(routes), AdminModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
