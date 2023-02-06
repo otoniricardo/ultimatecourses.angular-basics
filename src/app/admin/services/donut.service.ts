f5317c754ee792e37c941e7aa304d8f802f9a6ee
otoniricardo <otoniricardo@gmail.com>
2023-02-06 19:49:50 -0300

    [Services and API Requests]: Set HttpHeaders and RequestOptions

--- src/app/admin/services/donut.service.ts
+++ src/app/admin/services/donut.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 
 import {
   catchError,
@@ -26,7 +30,18 @@ export class DonutService {
     if (this.donuts.length) {
       return of(this.donuts);
     }
-    return this.http.get<Donut[]>(`/api/donuts`).pipe(
+
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    headers = headers.append('Api-Token', '1234abcd');
+
+    const options = {
+      headers,
+    };
+
+    return this.http.get<Donut[]>(`/api/donuts`, options).pipe(
       tap((donuts) => {
         this.donuts = donuts;
       }),
