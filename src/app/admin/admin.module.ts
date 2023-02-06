a62033d4177cfe37ad5e3f32caee418228104678
otoniricardo <otoniricardo@gmail.com>
2023-02-06 20:42:14 -0300

    [Routing and Navigation]: Declarative Navigation with routerLink Directi

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -22,7 +22,7 @@ export const routes: Routes = [
     component: DonutListComponent,
   },
   {
-    path: 'donut',
+    path: 'donuts/new',
     component: DonutSingleComponent,
   },
   {
--- src/app/admin/components/donut-card/donut-card.component.ts
+++ src/app/admin/components/donut-card/donut-card.component.ts
@@ -4,8 +4,9 @@ import { Donut } from '../../models/donut.model';
 @Component({
   selector: 'app-donut-card',
   template: `
-    <div
+    <a
       class="donut-card"
+      [routerLink]="donut.id"
       [ngClass]="{
         'donut-card-promo': donut.promo
       }"
@@ -34,7 +35,7 @@ import { Donut } from '../../models/donut.model';
           {{ donut.price / 100 | currency : 'BRL' : 'symbol' }}
         </p>
       </div>
-    </div>
+    </a>
   `,
   styles: [
     `
