f6b38b3b2a825136673278bd9eb990cda42a5376
otoniricardo <otoniricardo@gmail.com>
2023-02-06 13:51:51 -0300

    [Services and API Requests]: Configure and Proxy a Local HTTP Server

--- src/app/admin/services/donut.service.ts
+++ src/app/admin/services/donut.service.ts
@@ -5,45 +5,7 @@ import { Donut } from '../models/donut.model';
   providedIn: 'root',
 })
 export class DonutService {
-  private donuts: Donut[] = [
-    {
-      id: '1',
-      name: 'Just chocolate',
-      icon: 'just-chocolate',
-      price: 119,
-      description: 'For the pure chocolate',
-      promo: 'new',
-    },
-    {
-      id: '2',
-      name: 'Glazed fudge',
-      icon: 'glazed-fudge',
-      price: 129,
-      description: 'Sticky perfection',
-      promo: 'limited',
-    },
-    {
-      id: '3',
-      name: 'Caramel swirl',
-      icon: 'caramel-swirl',
-      price: 129,
-      description: 'Chocolate drizzled with caramel',
-    },
-    {
-      id: '4',
-      name: 'Sour supreme',
-      icon: 'sour-supreme',
-      price: 139,
-      description: 'For the sour advocate',
-    },
-    {
-      id: '5',
-      name: 'Zesty lemon',
-      icon: 'zesty-lemon',
-      price: 149,
-      description: 'Delicious lucious lemon',
-    },
-  ];
+  private donuts: Donut[] = [];
   constructor() {}
 
   read() {
